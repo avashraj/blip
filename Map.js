a3b84c24dddcd2d309232c38a9fb03ece40c9a21
avashraj <avashraj328@gmail.com>
2024-03-09 12:48:09 -0800

    login screen + components

--- Map.js
+++ Map.js
@@ -1,20 +1,19 @@
+// Map.js
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ProfileScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Profile Screen</Text>
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
-  container: {
+  mapContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
 });
 
-export default ProfileScreen;
+export default function Map() {
+  return (
+    <View style={styles.mapContainer}>
+      <Text>Map View Placeholder</Text>
+    </View>
+  );
+}
